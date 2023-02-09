b65d81fdb3c78fb670b90a0d81a6bd984c0b8646
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2023-02-09 21:02:20 +0500

    exercise 4

---
+++ 4_mad_lib.py
@@ -0,0 +1,7 @@
+noun = input('Enter a noun? ')
+verb = input('Enter a verb? ')
+adj = input('Enter an adjective? ')
+adv = input('Enter an adverb? ')
+
+print(f'Do you {verb} your {adj} {noun} {adv}? That\'s hilarious!')
+
